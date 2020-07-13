ae937f3f53945abc947a037b623e97e1245e5022
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-07-13 02:03:07 +0100

    Imroved header section

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -3,21 +3,37 @@
 		<div class="container p-3 p-md-4 d-flex justify-content-between align-items-center">
 			<div>
 				<a href="/">
-					<img class="border-circle" src="/icon.png" width="60" />
+					<i class="fas fa-home fa-3x"></i>
 				</a>
 			</div>
 
 			<div class="h1 mb-0">
-				<a class="mr-4" href="https://twitter.com/OgbeniHMMD">
+				<a class="mr-4" :href="link.contact">
+					<i class="fas fa-envelope"></i>
+				</a>
+				<a class="mr-4" :href="link.twitter">
 					<i class="fab fa-twitter"></i>
 				</a>
-				<a class="mr-4" href="https://t.me/OgbeniHMMD">
-					<i class="fab fa-telegram"></i>
-				</a>
-				<a class="mr-0" href="https://github.com/OgbeniHMMD">
+				<a class="mr-0" :href="link.github">
 					<i class="fab fa-github"></i>
 				</a>
 			</div>
 		</div>
 	</header>
 </template>
+
+
+<script>
+export default {
+	data: function() {
+		return {
+			link: {
+				twitter: process.env.link.twitter,
+				contact: process.env.link.contact,
+				telegram: process.env.link.telegram,
+				github: process.env.link.github
+			}
+		};
+	}
+};
+</script>
